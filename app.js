41ad2de11e7c6d0b5fedde431aa8dc70193468e5
Sakaz22 <90087474+Sakaz22@users.noreply.github.com>
2023-03-07 12:45:01 +0100

    Offsets added

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 document.documentElement.addEventListener("click", 
-function display(){console.log()}
+function display(){
+    let yOffset = document.getElementById("pageY");
+    let xOffset = document.getElementById("pageX");
+    yOffset.innerHTML = event.pageY;
+    xOffset.innerHTML = event.pageX;
+}
 )
